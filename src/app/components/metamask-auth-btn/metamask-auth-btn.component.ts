f365779f7ea6268f8ec2b9a313dc09a0fc475fdd
Cynan Rhodes <cr@cynanrhodes.com>
2023-09-15 12:17:15 +0200

    minor ui

--- src/app/components/metamask-auth-btn/metamask-auth-btn.component.ts
+++ src/app/components/metamask-auth-btn/metamask-auth-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getDefaultAccount } from 'src/app/shared/shared';
 import { EventService } from 'src/app/services/event.service';
