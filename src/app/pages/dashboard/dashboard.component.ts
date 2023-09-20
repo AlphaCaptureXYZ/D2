4e6d9ef3ff0040d4b0ad9eb0bd3b394273a962ff
Juan Batty <jmbl1685@hotmail.com>
2023-09-20 14:50:48 -0500

    more updates

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { EventService } from 'src/app/services/event.service';
 import { CommonModule } from '@angular/common';
 
 import { NFTCredentialService } from 'src/app/services/nft-credential.service';
+import { IPkpInfo, PKPGeneratorService } from 'src/app/services/pkp-generator.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,31 +16,47 @@ import { NFTCredentialService } from 'src/app/services/nft-credential.service';
   styleUrls: ['./dashboard.component.scss'],
 })
 export default class DashboardComponent implements OnInit {
+
   currentOption: string;
   isLoadingAccounts = false;
+  isLoadingMPCWallet = false;
+
   accounts: any[];
+  pkpInfo: IPkpInfo;
 
   constructor(
-      private eventService: EventService,
-      private nftCredentialService: NFTCredentialService,
-    ) {
+    private eventService: EventService,
+    private nftCredentialService: NFTCredentialService,
+    private pkpGeneratorService: PKPGeneratorService
+  ) {
     this.currentOption = 'dashboard';
     this.accounts = [];
-
+    this.pkpInfo = null as any;
   }
 
   async ngOnInit() {
     this.getAccounts();
+    this.getMPCWallet();
   }
 
   async getAccounts() {
     this.isLoadingAccounts = true;
     try {
       this.accounts = await this.nftCredentialService.getMyCredentials();
-    } catch(err) {
-      // console.log(err.message);
+    } catch (err) {
+      this.isLoadingAccounts = false;
     }
     this.isLoadingAccounts = false;
+  }
+
+  async getMPCWallet() {
+    this.isLoadingMPCWallet = true;
+    try {
+      this.pkpInfo = await this.pkpGeneratorService.getPKPInfo(); 
+    } catch (err) {
+      this.isLoadingMPCWallet = false;
+    }
+    this.isLoadingMPCWallet = false;
   }
 
 }
