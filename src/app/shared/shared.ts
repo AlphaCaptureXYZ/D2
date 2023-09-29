98e249f19056faf9be48135ff8b680814614cca7
Juan Batty <jmbl1685@hotmail.com>
2023-09-29 11:25:21 -0500

    trade banner added

--- src/app/shared/shared.ts
+++ src/app/shared/shared.ts
@@ -18,6 +18,14 @@ const valueToHexadecimal = (value: any) => {
   return response;
 };
 
+export const getTickerIcon = async (ticker: string) => {
+  const url = `https://technicals-rlco.ixily.io/v1/assets/${ticker}/logo`
+  const data = await fetch(url)
+  const response = await data.json()
+  const base64 = response?.data
+  return base64
+}
+
 const defaultChainInfo = {
   chainId: "0x13881",
   chainName: "Mumbai",
