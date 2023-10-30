67503b64930c1265a7f1d306518a2bc6fd271e17
Juan Batty <jmbl1685@hotmail.com>
2023-10-30 16:06:18 -0500

    IG Place Order - basic

--- src/app/scripts/lit-actions/parts/lit-action-ig.ts
+++ src/app/scripts/lit-actions/parts/lit-action-ig.ts
@@ -66,6 +66,144 @@ const checkCredentials = (
     return code;
 };
 
+const getAssetsBySymbol = (
+    env: EnvType,
+    symbol: string,
+    auth: {
+        apiKey: string,
+        cst: string,
+        securityToken: string,
+    }
+) => {
+    const requestUrl = getApiUrl(env);
+
+    const code = `
+        const go = async () => {
+
+            const url = '${requestUrl}/gateway/deal/markets?searchTerm=${symbol}';
+
+            const options = {
+                method: 'GET',
+                headers: {
+                    'Version': '1',
+                    'CST': '${auth.cst}',
+                    'X-IG-API-KEY': '${auth.apiKey}',
+                    'X-SECURITY-TOKEN': '${auth.securityToken}',
+                    'User-Agent': 'PostmanRuntime/7.29.2',
+                    'Accept': 'application/json; charset=UTF-8',
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                redirect: 'follow',
+                mode: 'cors',
+            };
+
+            const response = await fetch(url, options);
+            const data = await response.json();
+
+            const info = data?.markets || [];
+
+            Lit.Actions.setResponse({response: JSON.stringify(info)});
+
+        };
+
+        go();
+    `;
+
+    return code;
+}
+
+const placeOrder = (
+    env: EnvType,
+    orderPayload: {
+        direction: string,
+        epic: string,
+        quantity: number,
+    },
+    auth: {
+        apiKey: string,
+        cst: string,
+        securityToken: string,
+    }
+) => {
+    const requestUrl = getApiUrl(env);
+
+    const code = `
+        const go = async () => {
+
+            const url = '${requestUrl}/gateway/deal/positions/otc';
+
+            const dealReferenceGenerator = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+            const options = {
+                method: 'POST',
+                body: JSON.stringify({
+                    currencyCode: 'USD',
+                    dealReference: dealReferenceGenerator(),
+                    direction: '${orderPayload.direction.toUpperCase()}',
+                    epic: '${orderPayload.epic}',
+                    expiry: 'DFB',
+                    orderType: 'MARKET',
+                    size: '${orderPayload.quantity}',
+                    guaranteedStop: false,
+                    forceOpen: true,
+                }),
+                headers: {
+                    'Version': '2',
+                    'CST': '${auth.cst}',
+                    'X-IG-API-KEY': '${auth.apiKey}',
+                    'X-SECURITY-TOKEN': '${auth.securityToken}',
+                    'User-Agent': 'PostmanRuntime/7.29.2',
+                    'Accept': 'application/json; charset=UTF-8',
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                redirect: 'follow',
+                mode: 'cors',
+            };
+
+            const response = await fetch(url, options);
+            const data = await response.json();
+
+            const dealReference = data?.dealReference || null;
+
+            let globalResponse = null;
+
+            if(dealReference){
+                const orderDetailsReq = await fetch(
+                    '${requestUrl}/gateway/deal/confirms/' + dealReference,
+                    {
+                        method: 'GET',
+                        headers: {
+                            'Version': '1',
+                            'CST': '${auth.cst}',
+                            'X-IG-API-KEY': '${auth.apiKey}',
+                            'X-SECURITY-TOKEN': '${auth.securityToken}',
+                            'User-Agent': 'PostmanRuntime/7.29.2',
+                            'Accept': 'application/json; charset=UTF-8',
+                            'Content-Type': 'application/json; charset=UTF-8',
+                        },
+                        redirect: 'follow',
+                        mode: 'cors',
+                    }
+                );
+    
+                const orderDetails = await orderDetailsReq.json();
+                globalResponse = orderDetails;
+            } else {
+                globalResponse = data;
+            }
+
+            Lit.Actions.setResponse({response: JSON.stringify(globalResponse)});
+
+        };
+
+        go();
+    `;
+
+    return code;
+}
+
 export {
     checkCredentials,
+    getAssetsBySymbol,
+    placeOrder,
 };
