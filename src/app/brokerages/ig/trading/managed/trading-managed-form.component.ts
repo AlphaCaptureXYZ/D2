bf9bd0c1f07be3fc5cd8adead543b53104afecbf
Cynan Rhodes <cr@cynanrhodes.com>
2023-11-06 13:05:38 +0100

    IG managed trading form

--- src/app/brokerages/ig/trading/managed/trading-managed-form.component.ts
+++ src/app/brokerages/ig/trading/managed/trading-managed-form.component.ts
@@ -18,21 +18,15 @@ interface FormType {
   credentialNftUuid: string,
   broker: string;
   environment: string;
-  asset: string;
+  ticker: string;
   direction: string;
-  quantity: number;
+  // quantity: number;
   conviction: number;
-  value: number;
+  // value: number;
   orderLimits: boolean;
   maxLeverage: number;
   defaultOrderSize: number;
   maxSizePortofolio: number;
-
-  accountBalance: number;
-  defaultOrderValue: number;
-  maxPortofolioValue: number,
-  remainingPositionValue: number,
-
 }
 
 @Component({
@@ -49,41 +43,93 @@ export default class TradingManagedIGFormComponent implements OnInit {
   allAccounts: any[];
   broker = 'IG';
   credentials: any;
+  refreshLoading = false;
 
   isLoading = false as boolean;
   isLoadingCredentials = false as boolean;
 
+  orderSummaryMenu = [
+    false, // account settings
+    false, // asset
+    false, // order default settings
+    false, // order custom settings
+    false, // existing position
+    false, // validations
+    false, // potential order
+    true, // final adjusted order
+  ]
+
   data = {
+    asset: {
+        ticker: '',
+        price: {
+          ask: 0,
+          bid: 0,
+        },
+        minQty: 1,
+        fractional: false,
+        decimals: 1,
+    },
     account: {
         balance: 0,
         leverage: 1,
         leverageBalance: 0,
+        currencySymbol: '$',
     },
     existingPosition: {
         valueInBase: 0,
         currentPortfolioAllocation: 0,
+        remainingValue: 0,
     },
     order: {
       default: {
         value: 0,
         valueWithConviction: 0,
-        portfolioAllication: 0,
+        portfolioAllocation: 0,
+      },
+      settings: {
         conviction: 0,
+        maxPortfolioSize: 1,
+        maxPortfolioValue: 0,
+      },
+      calc: {
+        maxPortfolioValueExceeded: false,
+        maxPortfolioValueExceededBy: 0,
+        overrideLimits: false,
+        exceedsMinQty: true,
+      },
+      potential: {
+        direction: '',
+        value: 0,
+        quantity: 0,
+        orderSizePercentage: 0,
+        portfolio: {
+          value: 0,
+          allocation: 0,
+        },
+        price: {
+          value: 0,
+          type: '',
+        }
+      },
+      final: {
+        direction: '',
+        value: 0,
+        quantity: {
+          raw: 0,
+          rounded: 0,
+        },
+        orderSizePercentage: 0,
+        portfolio: {
+          value: 0,
+          allocation: 0,
+        },
+        price: {
+          value: 0,
+          type: '',
+        }
       }
     }
-
-    // valueInBase: 0,
-    // quantity: 0,
-    // calculation: {
-    //   base: {
-        
-    //   }
-    //     defaultOrderValueInBase: 0,
-    //     defaultOrderSizeAsPercent: 0,
-    //     maximumPositionValueInBase: 0,
-    //     maximumPositionSizeAsPercent: 0,
-    //     exceedsMaximum: false,
-    // }    
 }  
 
   constructor(
@@ -97,28 +143,33 @@ export default class TradingManagedIGFormComponent implements OnInit {
       credentialNftUuid: '',
       broker: '',
       environment: 'prod',
-      asset: '',
+      ticker: '',
       direction: 'buy',
-      quantity: 1,
+      // quantity: 1,
       conviction: 100,
-      value: 0,
+      // value: 0,
       orderLimits: false,
       maxLeverage: 1,
       defaultOrderSize: 2,
       maxSizePortofolio: 10,
-
-      accountBalance: 0,
-      defaultOrderValue: 0,
-      maxPortofolioValue: 0,
-      remainingPositionValue: 0,
     };
     this.allAccounts = [];
 
   }
 
   async ngOnInit() {
-    this.getCredentials();
+    // this.getCredentials();
+    this.getAccountBalanceAndPositions();
+    this.defaultOrderCalcUsingtheAccountBalance();
     this.callEvents();
+  }
+
+  refreshBrokerageData() {
+    this.refreshLoading = true;
+    this.getAccountBalanceAndPositions();
+    this.defaultOrderCalcUsingtheAccountBalance();
+    this.callEvents();
+    this.refreshLoading = false;
   }
 
   async getCredentials() {
@@ -129,6 +180,39 @@ export default class TradingManagedIGFormComponent implements OnInit {
     this.allAccounts = await this.nftCredentialService.getMyCredentials(pkpWalletAddress);
     this.allAccounts = this.allAccounts.filter(res => res.provider === this.broker);
     this.isLoadingCredentials = false;
+  }
+
+  async getAccountBalanceAndPositions() {
+    // call IG if we have credentials
+
+    const accountCurrencySymbol = '£';
+    this.data.account.currencySymbol = accountCurrencySymbol;
+
+
+    const accountBalance = 999;
+
+    this.data.account.balance = accountBalance;
+    this.data.account.leverageBalance = accountBalance * this.data.account.leverage;
+  
+    // get any existing position for the asset
+    const existingPositionValue = 40;
+
+    if (existingPositionValue) {
+      this.data.existingPosition.valueInBase = existingPositionValue;
+      this.data.existingPosition.currentPortfolioAllocation = this.data.existingPosition.valueInBase / this.data.account.leverageBalance * 100;
+    }
+  }
+
+  async getAsset() {
+    // call IG and get the asset price
+
+    // set our data...
+    this.data.asset.ticker = this.form.ticker;
+    this.data.asset.price.ask = 95;
+    this.data.asset.price.bid = 90;
+    this.data.asset.minQty = 1;  
+    this.data.asset.fractional = true;
+    this.data.asset.decimals = 1;
   }
 
   callEvents = () => {
@@ -143,9 +227,9 @@ export default class TradingManagedIGFormComponent implements OnInit {
             ...this.form,
             broker: data.pricingProvider,
             environment: 'prod',
-            asset: data.asset,
+            ticker: data.ticker,
             direction: 'buy',
-            quantity: 1,
+            // quantity: 1,
           };
           break;
       }
@@ -153,6 +237,7 @@ export default class TradingManagedIGFormComponent implements OnInit {
   };
 
   requiredControl = (valueChanged?: string): void => {
+    // console.log('valueChanged', valueChanged);
     const credentialNftUuid = this.form.credentialNftUuid;
     const account = this.allAccounts?.find(res => res.uuid === credentialNftUuid);
 
@@ -166,12 +251,19 @@ export default class TradingManagedIGFormComponent implements OnInit {
       this.calculatePortfolioValueInBaseCurrency();
     }
 
+    // get our asset details
+    this.getAsset();    
+
+    // this refreshes everything
+    this.refreshFormCalculation();
+
+    // replace this with a check for the order qty
     if (
       this.form.broker.length > 0 &&
-      this.form.asset.length > 0 &&
-      this.form.asset.length > 0 &&
-      this.form.direction.length > 0 &&
-      this.form.quantity > 0
+      this.form.ticker.length > 0 &&
+      this.form.ticker.length > 0 &&
+      this.form.direction.length > 0
+      // && this.form.quantity > 0
     ) {
       this.submitEnabled = true;
     } else {
@@ -244,82 +336,177 @@ export default class TradingManagedIGFormComponent implements OnInit {
     }
   }
 
-  accountBalanceLeveraged(accountBalance: number) {
-    // we should call this when we have the account balance from IG
-
-    // from IG after a call
-    this.data.account.balance = accountBalance;
-
-    // from the user input
-    this.data.account.balance = this.form.maxLeverage;
-
+  accountBalanceLeveraged() {
     // calculated leverage balance
-    this.data.account.leverageBalance = this.data.account.balance * this.data.account.balance;      
+    this.data.account.leverageBalance = this.data.account.balance * this.data.account.leverage;      
   }
 
-  defaultOrderCalc() {
+  defaultOrderCalcUsingtheAccountBalance() {
+
+    // updatye our override setting
+    this.data.order.calc.overrideLimits = this.form.orderLimits;
 
     // we're just calculating the default
-    this.data.order.default.value = this.form.defaultOrderSize * this.data.account.balance;
-    this.data.order.default.valueWithConviction = this.data.order.default.value * this.form.conviction;
-    this.data.order.default.conviction = this.form.conviction;
+    this.data.order.default.portfolioAllocation = this.form.defaultOrderSize;
+    this.data.order.default.value = (this.form.defaultOrderSize / 100) * this.data.account.balance;
+    this.data.order.default.valueWithConviction = this.data.order.default.value * (this.form.conviction / 100);
+    this.data.order.settings.conviction = this.form.conviction;
+
+    //
+    this.data.order.settings.maxPortfolioSize = this.form.maxSizePortofolio;
+    this.data.order.settings.maxPortfolioValue = (this.data.order.settings.maxPortfolioSize / 100) * this.data.account.leverageBalance;
+
+    // does the calculated value exceed the max?
+    // we need the combined value of the order and the existing portfolio value
+    const aggregatePosition = this.data.existingPosition.valueInBase + this.data.order.default.valueWithConviction;
+    if (aggregatePosition > this.data.order.settings.maxPortfolioValue) {
+      this.data.order.calc.maxPortfolioValueExceeded = true;
+      this.data.order.calc.maxPortfolioValueExceededBy = this.data.order.default.valueWithConviction - this.data.order.settings.maxPortfolioValue;
+    } else {
+      this.data.order.calc.maxPortfolioValueExceeded = false;
+      this.data.order.calc.maxPortfolioValueExceededBy = 0;
+    }
+
+
+    // our remaining position size (related to the existing portfolio position) needs to be calculated here
+    this.data.existingPosition.remainingValue = this.data.order.settings.maxPortfolioValue - this.data.existingPosition.valueInBase;
+
+    // PRE
+
+    // the potential order size is that before any vaidations are appliued
+    this.data.order.potential.value = this.data.order.default.valueWithConviction;
+    this.data.order.potential.portfolio.value = this.data.existingPosition.valueInBase + this.data.order.default.valueWithConviction;
+    this.data.order.potential.portfolio.allocation = this.data.order.potential.portfolio.value / this.data.account.leverageBalance * 100;
+
+    // qty needs to be worked out based on the price
+    // if the direction 'buy' we using the bid
+    this.data.order.potential.direction = this.form.direction;
+
+    // and sell, we use the ask
+    if (this.data.order.potential.direction.toLowerCase() === 'sell') {
+      this.data.order.potential.price.type = 'sell';
+      this.data.order.potential.price.value = this.data.asset.price.bid;
+    } else {
+      this.data.order.potential.price.type = 'buy';
+      this.data.order.potential.price.value = this.data.asset.price.ask;
+    }
+
+    // now we have a price, we can work out the qty
+
+    this.data.order.potential.quantity = this.data.order.potential.value / this.data.order.potential.price.value;
+
+    // FINAL
+    // work out our final values
+
+    // qty needs to be worked out based on the price
+    // if the direction 'buy' we using the bid
+    this.data.order.final.direction = this.form.direction;
+
+    // are we above the minimum qty
+    if (this.data.order.final.quantity.raw < this.data.asset.minQty) {
+      this.data.order.calc.exceedsMinQty = true;
+    } else {
+      this.data.order.calc.exceedsMinQty = false;
+    }
+
+    // and sell, we use the ask
+    if (this.data.order.final.direction.toLowerCase() === 'sell') {
+      this.data.order.final.price.type = 'sell';
+      this.data.order.final.price.value = this.data.asset.price.bid;
+    } else {
+      this.data.order.final.price.type = 'buy';
+      this.data.order.final.price.value = this.data.asset.price.ask;
+    }
+
+    // allow for the min qty and exceeding any portfolio values (or not)
+    // if it's below the minimum, then everything goes to zero
+    if (!this.data.order.calc.exceedsMinQty) {
+
+      this.data.order.final.value = 0;
+      this.data.order.final.quantity.raw = 0;
+      this.data.order.final.quantity.rounded = 0;
+      this.data.order.final.orderSizePercentage = 0;
+
+    } else {
+
+      // if the validations don't pass, we need to restrict the order value to the remaining position
+      // unless the user explcitly says to do otherwise
+      if (this.data.order.calc.maxPortfolioValueExceededBy) {
+
+        // if the user is overriding the settings we use the potential
+        if (!this.data.order.calc.overrideLimits) {
+
+          // use the remaining value
+          this.data.order.final.value = this.data.existingPosition.remainingValue;
+          this.data.order.final.orderSizePercentage = this.data.order.final.value / this.data.account.leverageBalance * 100;
+          this.data.order.final.portfolio.value = this.data.existingPosition.valueInBase + this.data.order.final.value;
+          this.data.order.final.portfolio.allocation = this.data.order.potential.portfolio.value / this.data.account.leverageBalance * 100;
+
+        } else {
+
+          // use the potential (without adjustment)
+          this.data.order.final.value = this.data.order.default.valueWithConviction;
+          this.data.order.final.orderSizePercentage = this.data.order.final.value / this.data.account.leverageBalance * 100;
+          this.data.order.final.portfolio.value = this.data.existingPosition.valueInBase + this.data.order.final.value;
+          this.data.order.final.portfolio.allocation = this.data.order.potential.portfolio.value / this.data.account.leverageBalance * 100;
+  
+        }
+
+      } else {
+
+        this.data.order.final.value = this.data.order.default.valueWithConviction;
+        this.data.order.final.orderSizePercentage = this.data.order.final.value / this.data.account.leverageBalance * 100;
+        this.data.order.final.portfolio.value = this.data.existingPosition.valueInBase + this.data.order.final.value;
+        this.data.order.final.portfolio.allocation = this.data.order.potential.portfolio.value / this.data.account.leverageBalance * 100;
+    
+      }
+
+      // now we have a price, we can work out the qty
+      this.data.order.final.quantity.raw = this.data.order.final.value / this.data.order.final.price.value;
+
+      // round the qty
+      if (this.data.asset.fractional) {
+        this.data.order.final.quantity.rounded = parseFloat(this.data.order.final.quantity.raw.toFixed(this.data.asset.decimals));
+      } else {
+        // if fraction isn't supported, then we need an int
+        this.data.order.final.quantity.rounded = Math.floor(this.data.order.final.quantity.raw);
+      }
+
+      // then we need to deal with custom qty and value
+      // if the validations all pass, then we can use our standard 
+
+
+    }
+
 
   }
 
+  refreshFormCalculation() {
 
-  async orderSizeCalculation(
-    accountBalanceInBase: number,
-    accountLeverage: number,
-    defaultOrderSizeAsPercent: number,
-    maximumPositionSizeAsPercent: number,
-    existingPositionValueInBase: number,
-    ) {
-    try {
+    // recalc the account leveraged account balance
+    this.accountBalanceLeveraged();
+    this.defaultOrderCalcUsingtheAccountBalance();
 
-        // let calculatedOrderValueInBase = 0;
-        // let remainingPositionValueInBase = 0;
-        // let currentPortfolioAllocation = 0;
-        // let exceedsMaximum = false;
+  }
 
-        // // we'll do a very simple calc to start i.e. new order and then I'll add the rest for adjust/close etc
+  expandCollapseOrderMenu(section: number) {
 
-        // // account balance in a single base currency
-        // // the leveraged balance is the maximum value the portfolio should be at
-        // const leveragedBalanceInBase = accountBalanceInBase * accountLeverage;
-
-        // // what a normal trade value should be
-        // const defaultOrderValueInBase = defaultOrderSizeAsPercent * leveragedBalanceInBase;
-        // // this becomes our default order value
-        // calculatedOrderValueInBase = defaultOrderValueInBase;
-
-        // // The existing position 
-        // const maximumPositionValueInBase = maximumPositionSizeAsPercent * leveragedBalanceInBase;
-
-        // // the current portfolio percentge
-        // currentPortfolioAllocation = (existingPositionValueInBase / leveragedBalanceInBase) * 100;
-
-        // // if the current asset value exceeds the maximum, then the 
-        // if (existingPositionValueInBase > maximumPositionValueInBase) {
-        //     calculatedOrderValueInBase = 0;
-        //     exceedsMaximum = true;
-        // } else {
-
-        //     // how much extra value can we buy
-        //     remainingPositionValueInBase = maximumPositionValueInBase - existingPositionValueInBase;
-
-        //     if (calculatedOrderValueInBase > remainingPositionValueInBase) {
-        //         calculatedOrderValueInBase = remainingPositionValueInBase;
-        //     } else {
-        //         // there's enough value for the default order
-        //     }
-
-        // }
-
-
-    } catch (err: any) {
-        console.log('orderSizeCalculation (error)', err?.message);
+    if (this.orderSummaryMenu[section]) {
+      this.orderSummaryMenu[section] = false;
+    } else {
+      this.orderSummaryMenu[section] = true;
     }
+  }
+
+  expandOrderSummary(action: boolean) {
+    for (const i in this.orderSummaryMenu) {
+      this.orderSummaryMenu[i] = action;
+    }
+  }
+
 }
 
-
-}
+// to do
+// select the IG asset
+// get positions
+// allow for the value of the existing portfolio
